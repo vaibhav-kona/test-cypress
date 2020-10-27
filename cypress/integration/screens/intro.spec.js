b4ab9fe47442264a03d4042edbeeabffc50e2be9
Vaibhav <vaibhav@bluesapling.com>
2020-10-27 12:18:52 +0530

    Check with a png image

--- cypress/integration/screens/intro.spec.js
+++ cypress/integration/screens/intro.spec.js
@@ -3,6 +3,6 @@ import Test1 from '../../../src/Test1';
 context('make screen load', () => {
   it('should work', () => {
     cy.visit('http://localhost:3009/');
-    cy.get('[data-cy=test-content]').should('be', 'This a jsx component.')
+    cy.get('[data-cy=test-content]').contains('This a jsx component.');
   })
 })
