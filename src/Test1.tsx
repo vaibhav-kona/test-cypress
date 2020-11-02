ab61209c97abf7ffd5fc328db6ae915a729e0d3c
Vaibhav <vaibhav@bluesapling.com>
2020-11-02 16:55:41 +0530

    Install version 4.5.0
    - PNG, svg, image and import of jsx into tsx is working correctly.

--- src/Test1.tsx
+++ src/Test1.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Test2 from './Test2';
+import img1 from '../src/logo512.png';
+import logo from '../src/logo.svg';
 
 const Test1 = () => {
   return (
     <>
       This has jsx component as an import.
       <Test2 />
+      <img src={img1} alt="img1" />
+      <img src={logo} className="App-logo" alt="logo" />
     </>
   );
 }
