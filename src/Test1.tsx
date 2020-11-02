5f2706f78b0cb0dd695e94f7796e283f4283bbae
Vaibhav <vaibhav@bluesapling.com>
2020-11-02 16:50:10 +0530

    Install version 4.4.0
    - Added png image and test seems to be working correctly.

--- src/Test1.tsx
+++ src/Test1.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Test2 from './Test2';
+import abcd from '../src/logo192.png';
 
 const Test1 = () => {
   return (
     <>
       This has jsx component as an import.
       <Test2 />
+      <img src={abcd} alt="hello" />
     </>
   );
 }
